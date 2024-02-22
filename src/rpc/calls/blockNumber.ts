d7cfc98aa720b794bc3f5d9dbd7b64db6c25eb6a
JimmyFate <jimmy.hyper.fate@gmail.com>
2024-02-22 20:07:45 +0100

    revert forwardRequest

--- src/rpc/calls/blockNumber.ts
+++ src/rpc/calls/blockNumber.ts
@@ -1,8 +1,27 @@
 import { RPCError, RPCRequest, RPCResponse } from '../../types/types'
-import { forwardRequest } from '../../utils/callHelper'
+import { callStarknet } from '../../utils/callHelper'
 
 export async function blockNumberHandler(
   request: RPCRequest,
 ): Promise<RPCResponse | RPCError> {
-  return forwardRequest(request, 'starknet_blockNumber', [])
+  const response: RPCResponse | string = await callStarknet('testnet', {
+    jsonrpc: request.jsonrpc,
+    method: 'starknet_blockNumber',
+    params: [],
+    id: request.id,
+  })
+
+  if (typeof response === 'string') {
+    return {
+      code: 7979,
+      message: 'Starknet RPC error',
+      data: response,
+    }
+  }
+
+  return {
+    jsonrpc: '2.0',
+    id: 1,
+    result: response.result,
+  }
 }
