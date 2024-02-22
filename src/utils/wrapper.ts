624add57f7849a47ceb90ec626b6c607a722a6d3
ermvrs <ermvrs@live.com>
2024-02-22 16:06:47 +0300

    wrapper functions

---
+++ src/utils/wrapper.ts
@@ -0,0 +1,9 @@
+export async function getEthAddressFromSnAddress(): Promise<string> {
+    // TODO: Will returns value directly from contract
+    return '0';
+}
+
+export async function getSnAddressFromEthAddress(): Promise<string> {
+    // TODO: Will returns value directly from contract
+    return '0';
+}
