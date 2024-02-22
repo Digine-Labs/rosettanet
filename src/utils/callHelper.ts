1175bce191a19297df8dae47785b471205708b7f
Tuan Tran <tropicaldog17@gmail.com>
2024-02-22 10:46:54 +0700

    add forwardRequest according to #29

--- src/utils/callHelper.ts
+++ src/utils/callHelper.ts
@@ -1,6 +1,10 @@
 import { getRpc } from './getRpc'
-import { RPCRequest, RPCResponse } from '../types/types'
+import { RPCRequest, RPCResponse, RPCError } from '../types/types'
+import { hexPadding } from './padding';
 import axios from 'axios'
+
+const GET_STORAGE_AT_METHOD = "starknet_getStorageAt";
+const ETH_GET_STORAGE_AT_RESULT_LENGTH = 64;
 
 export async function callStarknet(
   network: string,
@@ -25,5 +29,38 @@ export async function callStarknet(
     } else {
       return 'An unexpected error occurred'
     }
+  }
+}
+
+export async function forwardRequest(
+  request: RPCRequest,
+  method: string,
+  params: any[]
+): Promise<RPCResponse | RPCError> {
+  // TODO: dynamic network from env?
+  const network = 'testnet'
+  const response: RPCResponse | string = await callStarknet(network, {
+    jsonrpc: request.jsonrpc,
+    method,
+    params,
+    id: request.id,
+  })
+
+  if (typeof response === 'string') {
+    return {
+      code: 7979,
+      message: 'Starknet RPC error',
+      data: response,
+    }
+  }
+
+  if (method == GET_STORAGE_AT_METHOD){
+    response.result = hexPadding(response.result as string, ETH_GET_STORAGE_AT_RESULT_LENGTH);
+  }
+
+  return {
+    jsonrpc: '2.0',
+    id: 1,
+    result: response.result,
   }
 }
