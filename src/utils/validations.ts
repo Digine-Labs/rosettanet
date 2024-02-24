98ee09851d53449f551a81f55c10012f6aad0207
ktarun1419 <tarunkaushik355@gmail.com>
2024-02-24 23:46:51 +0530

    added address validations

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,10 +1,24 @@
+import { hexPadding } from './padding'
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export function validateEthAddress(ethAddress: string): boolean {
-  // TODO
+  if (!ethAddress) {
+    return false
+  }
+  let address: string = hexPadding(ethAddress, 40)
+  if (!address.match(/^(0x)?[0-9a-fA-F]{40}$/)) {
+    return false
+  }
   return true
 }
 
 export function validateSnAddress(snAddress: string): boolean {
-  // TODO
+  if (!snAddress) {
+    return false
+  }
+  let address: string = hexPadding(snAddress, 64)
+  if (!address.match(/^(0x)?[0-9a-fA-F]{64}$/)) {
+    return false
+  }
   return true
 }
