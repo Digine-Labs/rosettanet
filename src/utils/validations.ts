6fe3110c309ceeffeddbaaabc9ccc828fee1f791
ktarun1419 <tarunkaushik355@gmail.com>
2024-02-25 00:49:43 +0530

    added test cases

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,11 +1,11 @@
-import { hexPadding } from './padding'
+import { addHexPrefix, hexPadding, removeHexPrefix } from './padding'
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export function validateEthAddress(ethAddress: string): boolean {
   if (!ethAddress) {
     return false
   }
-  let address: string = hexPadding(ethAddress, 40)
+  let address: string = ethAddress?.toLowerCase()?.startsWith("0x") ? ethAddress : `0x${ethAddress}`
   if (!address.match(/^(0x)?[0-9a-fA-F]{40}$/)) {
     return false
   }
@@ -16,7 +16,7 @@ export function validateSnAddress(snAddress: string): boolean {
   if (!snAddress) {
     return false
   }
-  let address: string = hexPadding(snAddress, 64)
+  let address: string = addHexPrefix(removeHexPrefix(snAddress).padStart(64,"0"))
   if (!address.match(/^(0x)?[0-9a-fA-F]{64}$/)) {
     return false
   }
