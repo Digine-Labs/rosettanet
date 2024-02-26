42b8782ae4d6f2ef82de19decdbf3bc38b62ceed
Berzan <berzan@tutanota.com>
2024-02-26 11:58:14 +0000

    impl: `eth_getTransactionReceipt` test

--- tests/rpc/calls/getTransactionReceipt.test.ts
+++ tests/rpc/calls/getTransactionReceipt.test.ts
@@ -7,7 +7,7 @@ describe('Test get transaction receipt request testnet', () => {
       jsonrpc: '2.0',
       method: 'eth_getTransactionReceipt',
       params: [
-        '0xbb3a336e3f823ec18197f1e13ee875700f08f03e2cab75f0d0b118dabb44cba0',
+        '0x054b60a3d157a9b9a7d1734aaaf06db939ff5ae6b9c75e221cf8504f464ec0ef',
       ],
       id: 1,
     }
@@ -17,17 +17,63 @@ describe('Test get transaction receipt request testnet', () => {
 
     expect(typeof starkResult.result).toBe('object')
     expect(starkResult.result).toMatchObject({
-      transactionHash: '0xtodo',
-      blockHash: '0xtodo',
-      blockNumber: '0xtodo',
-      logs: [],
-      contractAddress: null,
+      transactionHash:
+        '0x54b60a3d157a9b9a7d1734aaaf06db939ff5ae6b9c75e221cf8504f464ec0ef',
+      blockHash:
+        '0x583a703ef5de667b9d19f87d5cc46ac140cc3ac5b3a2991555bd7c45fd58863',
+      blockNumber: '0xe95c8',
+      logs: [
+        {
+          address:
+            '0x52c6085f07d8381577703e656fd10dc1a95cdac6f7e8cf800a216606ffee4c7',
+          blockHash:
+            '0x583a703ef5de667b9d19f87d5cc46ac140cc3ac5b3a2991555bd7c45fd58863',
+          blockNumber: '0xe95c8',
+          data: '0xtodo',
+          logIndex: '0xtodo',
+          removed: false,
+          topics: [],
+          transactionHash:
+            '0x54b60a3d157a9b9a7d1734aaaf06db939ff5ae6b9c75e221cf8504f464ec0ef',
+          transactionIndex: '0xtodo',
+        },
+        {
+          address:
+            '0x52c6085f07d8381577703e656fd10dc1a95cdac6f7e8cf800a216606ffee4c7',
+          blockHash:
+            '0x583a703ef5de667b9d19f87d5cc46ac140cc3ac5b3a2991555bd7c45fd58863',
+          blockNumber: '0xe95c8',
+          data: '0xtodo',
+          logIndex: '0xtodo',
+          removed: false,
+          topics: [],
+          transactionHash:
+            '0x54b60a3d157a9b9a7d1734aaaf06db939ff5ae6b9c75e221cf8504f464ec0ef',
+          transactionIndex: '0xtodo',
+        },
+        {
+          address:
+            '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+          blockHash:
+            '0x583a703ef5de667b9d19f87d5cc46ac140cc3ac5b3a2991555bd7c45fd58863',
+          blockNumber: '0xe95c8',
+          data: '0xtodo',
+          logIndex: '0xtodo',
+          removed: false,
+          topics: [],
+          transactionHash:
+            '0x54b60a3d157a9b9a7d1734aaaf06db939ff5ae6b9c75e221cf8504f464ec0ef',
+          transactionIndex: '0xtodo',
+        },
+      ],
+      contractAddress:
+        '0x52c6085f07d8381577703e656fd10dc1a95cdac6f7e8cf800a216606ffee4c7',
       effectiveGasPrice: '0xtodo',
-      cumulativeGasUsed: '0xtodo',
+      cumulativeGasUsed: '0x30841e33136',
       from: '0xtodo',
-      gasUsed: '0xtodo',
+      gasUsed: '0x30841e33136',
       logsBloom: '0x0',
-      status: '0xtodo',
+      status: '0x1',
       to: '0xtodo',
       transactionIndex: '0xtodo',
       type: '0xtodo',
