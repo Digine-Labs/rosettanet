84f418abeb3c9cd89c571a348b6c897bad216bce
Yakup Altay <111201244+YakupAltay@users.noreply.github.com>
2024-06-25 14:11:34 +0300

    Update integer.ts

--- src/utils/converters/integer.ts
+++ src/utils/converters/integer.ts
@@ -4,21 +4,19 @@ import BigNumber from 'bignumber.js'
 // value has to be string of length 64 representation in hex. Remove 0x prefix
 export function Uint256ToU256(value: string): string | Array<string> {
   if (value.length != 64) {
-    return ['0', '0']
+    return ['0', '0'];
   }
 
   const high: string = new BigNumber(value.substring(0, 32), 16).toString(16)
   const low: string = new BigNumber(value.substring(32, 64), 16).toString(16)
-  return [low, high]
+  return [low, high];
 }
 
 export function U256toUint256(value: Array<string>): string {
-  const low = BigInt(value[0])
-  const high = BigInt(value[1])
+  const low = new BigNumber(value[0], 16).toString(16).padStart(32, '0')
+  const high = new BigNumber(value[1], 16).toString(16).padStart(32, '0')
 
-  const result = (high << BigInt(64)) + low
-
-  return '0x' + result.toString(16)
+  return '0x' + high + low;
 }
 
 // TODO: support signed integers
