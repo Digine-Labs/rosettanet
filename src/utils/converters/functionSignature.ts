ffc353c1cb3bb6bacf81cb203f93c88c76e4a046
Erim <ermvrs@live.com>
2024-10-02 13:46:34 +0300

    fmt

--- src/utils/converters/functionSignature.ts
+++ src/utils/converters/functionSignature.ts
@@ -6,8 +6,8 @@ import { Abi } from 'starknet'
 import { StarknetTypeMember } from '../../types/types'
 
 interface SolidityType {
-  type: string,
-  value: string | Array<string>,
+  type: string
+  value: string | Array<string>
   formatted?: string
 }
 
@@ -15,18 +15,27 @@ interface SolidityType {
 // https://ethereum.stackexchange.com/questions/137436/what-is-a-functions-function-signature-if-it-uses-a-custom-type-stuct-enum
 
 const elementaryTypes = [
-  ['core::felt252', {type: 'basic', value: 'uint256'}],
-  ['core::integer::u8', {type: 'basic', value: 'uint8'}],
-  ['core::integer::u16', {type: 'basic', value: 'uint16'}],
-  ['core::integer::u32', {type: 'basic', value: 'uint32'}],
-  ['core::integer::u64', {type: 'basic', value: 'uint64'}],
-  ['core::integer::u128', {type: 'basic', value: 'uint128'}],
-  ['core::integer::u256', {type: 'basic', value: 'uint256'}],
-  ['core::integer::usize', {type: 'basic', value: 'uint256'}],
-  ['core::starknet::contract_address::ContractAddress', {type: 'basic', value: 'uint256'}], //TODO: address or u256?
-  ['core::starknet::eth_address::EthAddress', {type: 'basic', value: 'address'}],
-  ['core::starknet::class_hash::ClassHash', {type: 'basic', value: 'uint256'}],
-  ['bool', {type: 'basic', value: 'bool'}],
+  ['core::felt252', { type: 'basic', value: 'uint256' }],
+  ['core::integer::u8', { type: 'basic', value: 'uint8' }],
+  ['core::integer::u16', { type: 'basic', value: 'uint16' }],
+  ['core::integer::u32', { type: 'basic', value: 'uint32' }],
+  ['core::integer::u64', { type: 'basic', value: 'uint64' }],
+  ['core::integer::u128', { type: 'basic', value: 'uint128' }],
+  ['core::integer::u256', { type: 'basic', value: 'uint256' }],
+  ['core::integer::usize', { type: 'basic', value: 'uint256' }],
+  [
+    'core::starknet::contract_address::ContractAddress',
+    { type: 'basic', value: 'uint256' },
+  ], //TODO: address or u256?
+  [
+    'core::starknet::eth_address::EthAddress',
+    { type: 'basic', value: 'address' },
+  ],
+  [
+    'core::starknet::class_hash::ClassHash',
+    { type: 'basic', value: 'uint256' },
+  ],
+  ['bool', { type: 'basic', value: 'bool' }],
 ] // These types can be directly renamed to generate ethereum signature
 
 // Inputs starknet abi string returning from getContractClass call
@@ -42,8 +51,8 @@ export function generateEthereumFunctionSignatures(
 
   // convertableTypesı kullanarak yeni mapping oluştur. Sonrasında bu mappingi, abi ın içinde bulunan custom structlar ile güncelle
 
-  const typeConversions: Map<string, SolidityType> = initializeConvertableTypes()
-
+  const typeConversions: Map<string, SolidityType> =
+    initializeConvertableTypes()
 
   importCustomStructs(classABI)
   return []
@@ -53,7 +62,7 @@ function initializeConvertableTypes(): Map<string, SolidityType> {
   const mapping: Map<string, SolidityType> = new Map<string, SolidityType>()
 
   for (const val of elementaryTypes) {
-    if(typeof val[0] === 'string' && typeof val[1] !== 'string') {
+    if (typeof val[0] === 'string' && typeof val[1] !== 'string') {
       mapping.set(val[0], val[1])
     }
   }
@@ -61,21 +70,24 @@ function initializeConvertableTypes(): Map<string, SolidityType> {
   return mapping
 }
 
-function generateTupleFormat(input: SolidityType, typeMapping: Map<string, SolidityType>): string {
-  if(input.type !== 'struct') {
-    return '';
+function generateTupleFormat(
+  input: SolidityType,
+  typeMapping: Map<string, SolidityType>,
+): string {
+  if (input.type !== 'struct') {
+    return ''
   }
-  let tupleFormat = '(';
-  let index = 0;
-  for(const curr  of input.value) {
-    if(!typeMapping.has(curr)) {
+  let tupleFormat = '('
+  let index = 0
+  for (const curr of input.value) {
+    if (!typeMapping.has(curr)) {
       // TODO: error handling, type not found
       // Eğer type yoksa hata ver, içerisindeki struct daha yüklenmemiş demek
-      index++;
-      continue;
+      index++
+      continue
     }
     const solidityEquivalent = typeMapping.get(curr)
-    if(index == input.value.length -1) {
+    if (index == input.value.length - 1) {
       tupleFormat = `${tupleFormat})`
     } else {
       tupleFormat = `${tupleFormat}${solidityEquivalent},`
@@ -110,8 +122,8 @@ function importCustomStructs(classABI: Abi): Array<any> {
     const structMembers = (elem.members as Array<StarknetTypeMember>).map(
       x => x.type,
     )
-    structArray.push({type: 'struct', value: structMembers})
-    
+    structArray.push({ type: 'struct', value: structMembers })
+
     structs.push(structArray)
     // Burda custom structların memberleri array şeklinde var. Bunları tuple çevireceğiz ama önce ethereum haline mi çevirsek??
   }
