cadc6b25c803caeeb47d5ae0d6de328086510841
Erim <ermvrs@live.com>
2024-10-30 13:54:33 +0300

    new chunk split function

--- src/utils/felt.ts
+++ src/utils/felt.ts
@@ -1,6 +1,15 @@
 // Returns array of hexdecimal strings splitted into specified chunksize.
 export function convertHexChunkIntoFeltArray(chunk: string): Array<string> {
   const chunkSize = 62 // Reduce to 62 if it can be higher than felt252 limit
+  return convertChunkIntoHexStringArray(chunk, chunkSize);
+}
+
+export function convertHextIntoBytesArray(chunk:string): Array<string> {
+  const chunkSize = 2
+  return convertChunkIntoHexStringArray(chunk, chunkSize);
+}
+
+function convertChunkIntoHexStringArray(chunk: string, size: number): Array<string> {
   let hex = chunk
   if (chunk.indexOf('0x') > -1) {
     hex = chunk.slice(2)
@@ -8,8 +17,8 @@ export function convertHexChunkIntoFeltArray(chunk: string): Array<string> {
 
   const chunks: Array<string> = []
 
-  for (let i = 0; i < hex.length; i += chunkSize) {
-    chunks.push(`0x${hex.slice(i, i + chunkSize)}`)
+  for (let i = 0; i < hex.length; i += size) {
+    chunks.push(`0x${hex.slice(i, i + size)}`)
   }
 
   return chunks
