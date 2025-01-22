7c6b2ed685190c9776831ca72b82be0420da2489
ermvrs <ermvrs@live.com>
2025-01-22 17:17:49 +0300

    est gas multicall

--- src/rpc/calls/estimateGas.ts
+++ src/rpc/calls/estimateGas.ts
@@ -31,11 +31,34 @@ import { addHexPrefix } from '../../utils/padding'
 import { safeUint256ToU256 } from '../../utils/converters/integer'
 
 export async function estimateGasHandler(request: RPCRequest): Promise<RPCResponse|RPCError> {
-  return <RPCResponse> {
-    jsonrpc: request.jsonrpc,
-    id: request.id,
-    result: '0x5208'
+  try {
+    const parameters = request.params[0];
+    const from = parameters.from == null ? parameters.to : parameters.from;
+    const to = parameters.to;
+    //const targetFunctionSelector: string | null = getFunctionSelectorFromCalldata(parameters.data)
+
+    if(from === to && from != null) {
+      return <RPCResponse> {
+        jsonrpc: request.jsonrpc,
+        id: request.id,
+        result: '0x15F90'
+      }
+    } else {
+      return <RPCResponse> {
+        jsonrpc: request.jsonrpc,
+        id: request.id,
+        result: '0x5208'
+      }
+    }
+  } catch (ex) {
+    return <RPCResponse> {
+      jsonrpc: request.jsonrpc,
+      id: request.id,
+      result: '0x5208'
+    }
   }
+
+
 }
 
 export async function estimateGasHandlerx(request: RPCRequest): Promise<RPCResponse | RPCError> {
