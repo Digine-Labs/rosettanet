ecbfece02a9a83c67aca46da90f6cd5946afa673
Erim <ermvrs@live.com>
2025-02-20 13:29:14 +0300

    rm old devnet

---
+++ e2e/index.ts
@@ -0,0 +1,8 @@
+import { Devnet } from "starknet-devnet";
+
+ async function startDevnet() {
+    // Specify anything from https://github.com/0xSpaceShard/starknet-devnet-rs/releases
+    // Be sure to include the 'v' if it's in the version name.
+    const devnet = await Devnet.spawnVersion("latest");
+    console.log(await devnet.provider.isAlive()); // true
+}
