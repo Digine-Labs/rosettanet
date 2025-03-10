02336282bdc6d2d5c99e0ba8eb2aa51747b7d642
ermvrs <ermvrs@live.com>
2025-03-10 12:48:50 +0300

    limit gas

--- src/rpc/calls/estimateGas.ts
+++ src/rpc/calls/estimateGas.ts
@@ -5,24 +5,11 @@ export async function estimateGasHandler(
   request: RPCRequest,
 ): Promise<RPCResponse | RPCError> {
   try {
-    const parameters = request.params[0]
-    const from = parameters.from == null ? parameters.to : parameters.from
-    const to = parameters.to
-    //const targetFunctionSelector: string | null = getFunctionSelectorFromCalldata(parameters.data)
-
-    if (from === to && from != null) {
-      return <RPCResponse>{
-        jsonrpc: request.jsonrpc,
-        id: request.id,
-        result: '0x15F90',
-      }
-    } else {
       return <RPCResponse>{
         jsonrpc: request.jsonrpc,
         id: request.id,
         result: '0x5208',
       }
-    }
   } catch (ex) {
     return <RPCResponse>{
       jsonrpc: request.jsonrpc,
