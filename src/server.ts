2387900a6804d6a08fdb5e142fae10391b068583
Bora Atik <90131638+Boreas09@users.noreply.github.com>
2025-03-14 11:32:38 +0300

    Update server.ts
    cors place changed

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,11 @@ export function StartListening() {
   const host = getConfigurationProperty('host')
   const port = Number(getConfigurationProperty('port')) || 3000
 
+  app.use(cors())
+  app.options('*', cors())
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
   app.use(parseRequest)
-  app.use(cors())
-  app.options('*', cors())
   app.use('/', Routes)
 
   const server = app.listen(port, host, (): void => {
