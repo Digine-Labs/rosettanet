f5d2b229dc52db30396aebb2f9759517ed9dd802
ermvrs <ermvrs@live.com>
2025-04-03 17:27:05 +0300

    createaccesslist test

---
+++ e2e/rpc/createAccessList.test.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { SERVER } from '../utils'
+describe('eth_accounts RPC method', () => {
+    test.only('Should return empty array since access list is not supported with Rosettanet', async () => {
+        const response = await axios.post(SERVER, {
+            jsonrpc: '2.0',
+            method: 'eth_createAccessList',
+            params: [],
+            id: 1,
+        })
+        
+        expect(response.status).toBe(200)
+        expect(response.data.result).toBeDefined()
+        expect(response.data.result).toStrictEqual({accessList: []})
+        expect(response.data.jsonrpc).toBe('2.0')
+        expect(response.data.id).toBe(1)
+    }, 30000)
+})
