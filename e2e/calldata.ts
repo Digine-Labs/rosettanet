922078a768def3f28a02c154e51cfac69b346a71
Erim <ermvrs@live.com>
2025-04-03 20:13:17 +0300

    rm unused fn

--- e2e/calldata.ts
+++ e2e/calldata.ts
@@ -1,14 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AbiCoder, id, Interface } from "ethers";
+import { Interface } from "ethers";
 
-export function prepareCalldata(fn: string, inputs: any[]): string {
-    const abicoder = new AbiCoder();
-
-    const calldata = abicoder.encode([fn],[inputs])
-    const selector = id(fn).slice(0, 10);
-
-    return `${selector}${calldata}`
-}
 
 export function encodeCalldata(funcSignature: string, args: any) {
     const iface = new Interface([`function ${funcSignature}`]);
